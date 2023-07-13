4afc12cc835ea2eea794a65efaee00f24265fa7a
Jaimilit <>
2023-07-13 12:21:40 +0000

    create basics of boards- rows & columns and computer randomly chooses 5 spaces

--- run.py
+++ run.py
@@ -25,9 +25,9 @@ def print_board(board):
 
 
 def create_ships(board):
-"""
+    """
 create 5 random ships on board
-"""
+    """
     for ship in range(5):
         ship_row, ship_column = randint(0,7), randint(0,7)
         while board[ship_row][ship_column] == 'X':
@@ -35,11 +35,37 @@ create 5 random ships on board
         board[ship_row][ship_column] = 'X'
 
 def get_ship_location():
-    pass
+    """
+    choose ship location, row & column
+    """
+    row = input('Enter a ship row 1 - 8')
+    while row not in '12345678':
+        print('Please enter a valid row')
+        row = input('Enter a ship row 1 - 8')
+    column = input('Enter a ship column A - H').upper()
+    while column not in 'ABCDEFGH':
+        print('Please print a valid column')
+        column = input('Enter a ship column A - H').upper()
+    return int(row) - 1, letters_to_numbers[column]
 
-def count_hit_ships():
-    pass 
+def count_hit_ships(board):
+    """
+    count the ships hit in order to win/end the game
+    to check if X exists
+    """ 
+    count = 0
+    for row in board:
+        for column in row:
+            if column == 'X':
+                count += 1
+    return count
 
-create_ships()
+
+create_ships(HIDDEN_BOARD)
+"""
+create ships on hidden/opponent's board
+"""
 turns = 10
+print_board(HIDDEN_BOARD)
+print_board(GUESS_BOARD)
 # while turns > 0:
